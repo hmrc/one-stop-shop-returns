6c3912c35511169a9a747b0e7e71bc634de2ffd8
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2022-01-25 15:45:50 +0000

    VEOSS-800| updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,21 +6,21 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.14.0",
-    "uk.gov.hmrc"             %% "domain"                     % "6.2.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.55.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.20.0",
+    "uk.gov.hmrc"             %% "domain"                     % "7.0.0-play-28",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.59.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.14.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.55.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.20.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.59.0",
     "org.scalatest"           %% "scalatest"                  % "3.2.10",
     "com.typesafe.play"       %% "play-test"                  % PlayVersion.current,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
-    "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.10.0",
+    "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"                % "3.2.10.0",
-    "org.mockito"             %% "mockito-scala"              % "1.16.42",
-    "com.github.tomakehurst"  %  "wiremock-standalone"        % "2.25.0"
+    "org.mockito"             %% "mockito-scala"              % "1.17.0",
+    "com.github.tomakehurst"  %  "wiremock-standalone"        % "2.27.2"
   ).map(_ % "test, it")
 }
