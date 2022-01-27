e55d16c116e99399e0b818bab52747eab2624644
DevAndyroo <andy@andyroo.info>
2022-01-27 14:45:25 +0000

    VEOSS-777 Add test for toggle

--- test/services/HistoricalReturnSubmitServiceSpec.scala
+++ test/services/HistoricalReturnSubmitServiceSpec.scala
@@ -3,23 +3,17 @@ package services
 import base.SpecBase
 import config.AppConfig
 import connectors.{CoreVatReturnConnector, RegistrationConnector}
-import models._
-import models.core._
-import models.corrections.{CorrectionPayload, CorrectionToCountry, PeriodWithCorrections}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchersSugar.eqTo
 import org.mockito.Mockito
 import org.mockito.Mockito.when
-import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.BeforeAndAfterEach
 import testutils.RegistrationData
 import uk.gov.hmrc.domain.Vrn
 import uk.gov.hmrc.http.HeaderCarrier
 
-import java.time.Instant
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.math.BigDecimal.RoundingMode
 import scala.util.{Failure, Success}
 
 class HistoricalReturnSubmitServiceSpec extends SpecBase with BeforeAndAfterEach {
@@ -38,64 +32,78 @@ class HistoricalReturnSubmitServiceSpec extends SpecBase with BeforeAndAfterEach
 
   "HistoricalReturnSubmitService#transfer" - {
 
-    when(vatReturnService.get()) thenReturn Future.successful(List.empty)
-    when(appConfig.coreVatReturnsEnabled) thenReturn true
+    "when core vat return feature is enabled" - {
 
-    val service = new HistoricalReturnSubmitServiceImpl(vatReturnService, correctionService, coreVatReturnService, coreVatReturnConnector, registrationConnector, appConfig, stubClock)
+      when(vatReturnService.get()) thenReturn Future.successful(List.empty)
+      when(appConfig.coreVatReturnsEnabled) thenReturn true
 
-    "successfully transfer all data" in {
-      when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn))
-      when(correctionService.get()) thenReturn Future.successful(List(emptyCorrectionPayload))
-      when(coreVatReturnService.toCore(any(), any(), any())) thenReturn Future.successful(coreVatReturn)
-      when(coreVatReturnConnector.submit(any())) thenReturn Future.successful(Right())
-      when(registrationConnector.getRegistration(any())) thenReturn Future.successful(Some(RegistrationData.registration))
+      val service = new HistoricalReturnSubmitServiceImpl(vatReturnService, correctionService, coreVatReturnService, coreVatReturnConnector, registrationConnector, appConfig, stubClock)
 
-      service.transfer().futureValue mustBe Seq(Success(Right()))
+      "successfully transfer all data" in {
+        when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn))
+        when(correctionService.get()) thenReturn Future.successful(List(emptyCorrectionPayload))
+        when(coreVatReturnService.toCore(any(), any(), any())) thenReturn Future.successful(coreVatReturn)
+        when(coreVatReturnConnector.submit(any())) thenReturn Future.successful(Right())
+        when(registrationConnector.getRegistration(any())) thenReturn Future.successful(Some(RegistrationData.registration))
+
+        service.transfer().futureValue mustBe Seq(Success(Right()))
+      }
+
+      "don't fail future when partly fails" in {
+        val completeVatReturn2 = completeVatReturn.copy(vrn = Vrn("987654321"))
+        val completeVatReturn3 = completeVatReturn.copy(vrn = Vrn("987654322"))
+        val genericException = new Exception("Error")
+
+        when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn, completeVatReturn2, completeVatReturn3))
+        when(correctionService.get()) thenReturn Future.successful(List(emptyCorrectionPayload))
+        when(coreVatReturnService.toCore(eqTo(completeVatReturn), any(), any())) thenReturn Future.successful(coreVatReturn)
+        when(coreVatReturnService.toCore(eqTo(completeVatReturn2), any(), any())) thenReturn Future.failed(genericException)
+        when(coreVatReturnService.toCore(eqTo(completeVatReturn3), any(), any())) thenReturn Future.successful(coreVatReturn)
+        when(coreVatReturnConnector.submit(any())) thenReturn Future.successful(Right())
+        when(registrationConnector.getRegistration(any())) thenReturn Future.successful(Some(RegistrationData.registration))
+
+        service.transfer().futureValue mustBe Seq(Success(Right()), Failure(genericException), Success(Right()))
+      }
+
+      "fail gracefully when toCore fails" in {
+        val genericException = new Exception("Error")
+
+        when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn))
+        when(correctionService.get()) thenReturn Future.successful(List(emptyCorrectionPayload))
+        when(coreVatReturnService.toCore(eqTo(completeVatReturn), any(), any())) thenReturn Future.failed(genericException)
+
+        service.transfer().futureValue mustBe Seq(Failure(genericException))
+      }
+
+      "fail gracefully when vat return service fails" in {
+        val genericException = new Exception("Error")
+
+        when(vatReturnService.get()) thenReturn Future.failed(genericException)
+
+        service.transfer().futureValue mustBe ()
+      }
+
+      "fail gracefully when correction return service fails" in {
+        val genericException = new Exception("Error")
+
+        when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn))
+        when(correctionService.get()) thenReturn Future.failed(genericException)
+
+        service.transfer().futureValue mustBe ()
+      }
+
     }
 
-    "don't fail future when partly fails" in {
-      val completeVatReturn2 = completeVatReturn.copy(vrn = Vrn("987654321"))
-      val completeVatReturn3 = completeVatReturn.copy(vrn = Vrn("987654322"))
-      val genericException = new Exception("Error")
+    "when core vat return feature is disabled" - {
+      when(appConfig.coreVatReturnsEnabled) thenReturn true
 
-      when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn, completeVatReturn2, completeVatReturn3))
-      when(correctionService.get()) thenReturn Future.successful(List(emptyCorrectionPayload))
-      when(coreVatReturnService.toCore(eqTo(completeVatReturn), any(), any())) thenReturn Future.successful(coreVatReturn)
-      when(coreVatReturnService.toCore(eqTo(completeVatReturn2), any(), any())) thenReturn Future.failed(genericException)
-      when(coreVatReturnService.toCore(eqTo(completeVatReturn3), any(), any())) thenReturn Future.successful(coreVatReturn)
-      when(coreVatReturnConnector.submit(any())) thenReturn Future.successful(Right())
-      when(registrationConnector.getRegistration(any())) thenReturn Future.successful(Some(RegistrationData.registration))
+      val service = new HistoricalReturnSubmitServiceImpl(vatReturnService, correctionService, coreVatReturnService, coreVatReturnConnector, registrationConnector, appConfig, stubClock)
 
-      service.transfer().futureValue mustBe Seq(Success(Right()), Failure(genericException), Success(Right()))
+      "return empty future unit and do nothing" in {
+        service.transfer().futureValue mustBe ()
+      }
     }
-
-    "fail gracefully when toCore fails" in {
-      val genericException = new Exception("Error")
-
-      when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn))
-      when(correctionService.get()) thenReturn Future.successful(List(emptyCorrectionPayload))
-      when(coreVatReturnService.toCore(eqTo(completeVatReturn), any(), any())) thenReturn Future.failed(genericException)
-
-      service.transfer().futureValue mustBe Seq(Failure(genericException))
-    }
-
-    "fail gracefully when vat return service fails" in {
-      val genericException = new Exception("Error")
-
-      when(vatReturnService.get()) thenReturn Future.failed(genericException)
-
-      service.transfer().futureValue mustBe ()
-    }
-
-    "fail gracefully when correction return service fails" in {
-      val genericException = new Exception("Error")
-
-      when(vatReturnService.get()) thenReturn Future.successful(List(completeVatReturn))
-      when(correctionService.get()) thenReturn Future.failed(genericException)
-
-      service.transfer().futureValue mustBe ()
-    }
-
   }
+
 
 }
