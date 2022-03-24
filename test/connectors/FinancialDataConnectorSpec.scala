3afe942504eb414e55c1546354d70b8b4331f16c
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2022-03-24 12:06:05 +0000

    VEOSS-920| added http exception handling to financial data connector

--- test/connectors/FinancialDataConnectorSpec.scala
+++ test/connectors/FinancialDataConnectorSpec.scala
@@ -6,7 +6,11 @@ import com.github.tomakehurst.wiremock.matching.EqualToPattern
 import models.financialdata.{FinancialData, FinancialDataQueryParameters, FinancialTransaction, Item}
 import models.Period
 import models.Quarter.Q3
+import models.des.{DesErrorResponse, UnexpectedResponseStatus}
+import org.scalatest.concurrent.PatienceConfiguration.Timeout
+import org.scalatest.time.{Seconds, Span}
 import play.api.Application
+import play.api.http.Status.{BAD_REQUEST, IM_A_TEAPOT, NOT_FOUND, SERVICE_UNAVAILABLE}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers.running
 
@@ -15,6 +19,7 @@ import java.time.{LocalDate, ZonedDateTime}
 class FinancialDataConnectorSpec extends SpecBase with WireMockHelper {
 
   private val desUrl = s"/one-stop-shop-returns-stub/enterprise/financial-data/VRN/${vrn.value}/ECOM"
+  private val queryParameters = FinancialDataQueryParameters(fromDate = Some(period.firstDay), toDate = Some(LocalDate.now()))
 
   private def application: Application =
     new GuiceApplicationBuilder()
@@ -86,8 +91,6 @@ class FinancialDataConnectorSpec extends SpecBase with WireMockHelper {
           )
         )
 
-        val queryParameters = FinancialDataQueryParameters(fromDate = Some(period.firstDay), toDate = Some(LocalDate.now()))
-
         server.stubFor(
           get(urlEqualTo(desUrl + s"?dateFrom=${period.firstDay.toString}&dateTo=${queryParameters.toDate.get.toString}"))
             .withQueryParam("dateFrom", new EqualToPattern(period.firstDay.toString))
@@ -111,6 +114,77 @@ class FinancialDataConnectorSpec extends SpecBase with WireMockHelper {
           result mustEqual expectedResult
         }
       }
+    }
+
+    "must return None when server returns Not Found" in {
+        server.stubFor(
+          get(urlEqualTo(desUrl + s"?dateFrom=${period.firstDay.toString}&dateTo=${queryParameters.toDate.get.toString}"))
+            .withQueryParam("dateFrom", new EqualToPattern(period.firstDay.toString))
+            .withQueryParam("dateTo", new EqualToPattern(LocalDate.now.toString))
+            .withHeader("Authorization", equalTo("Bearer auth-token"))
+            .withHeader("Environment", equalTo("test-environment"))
+            .willReturn(
+              aResponse()
+                .withStatus(NOT_FOUND)
+            )
+        )
+
+        running(application) {
+          val connector = application.injector.instanceOf[FinancialDataConnector]
+          val result = connector.getFinancialData(vrn, queryParameters).futureValue
+          result mustBe Right(None)
+        }
+
+    }
+
+    "must return DesErrorResponse" - {
+      "when server returns Http Exception" in {
+        server.stubFor(
+          get(urlEqualTo(desUrl + s"?dateFrom=${period.firstDay.toString}&dateTo=${queryParameters.toDate.get.toString}"))
+            .withQueryParam("dateFrom", new EqualToPattern(period.firstDay.toString))
+            .withQueryParam("dateTo", new EqualToPattern(LocalDate.now.toString))
+            .withHeader("Authorization", equalTo("Bearer auth-token"))
+            .withHeader("Environment", equalTo("test-environment"))
+            .willReturn(
+              aResponse()
+                .withStatus(504)
+                .withFixedDelay(21000)
+            )
+        )
+
+        running(application) {
+          val connector = application.injector.instanceOf[FinancialDataConnector]
+          whenReady(connector.getFinancialData(vrn, queryParameters), Timeout(Span(30, Seconds))) { exp =>
+            exp.isLeft mustBe true
+            exp.left.get mustBe a[DesErrorResponse]
+          }
+
+        }
+      }
+
+      Seq(BAD_REQUEST, SERVICE_UNAVAILABLE, IM_A_TEAPOT).foreach{
+        status =>
+          s"when server returns status $status" in {
+            server.stubFor(
+              get(urlEqualTo(desUrl + s"?dateFrom=${period.firstDay.toString}&dateTo=${queryParameters.toDate.get.toString}"))
+                .withQueryParam("dateFrom", new EqualToPattern(period.firstDay.toString))
+                .withQueryParam("dateTo", new EqualToPattern(LocalDate.now.toString))
+                .withHeader("Authorization", equalTo("Bearer auth-token"))
+                .withHeader("Environment", equalTo("test-environment"))
+                .willReturn(
+                  aResponse()
+                    .withStatus(status)
+                )
+            )
+
+            running(application) {
+              val connector = application.injector.instanceOf[FinancialDataConnector]
+              val result = connector.getFinancialData(vrn, queryParameters).futureValue
+              result mustBe Left(UnexpectedResponseStatus(status, s"Unexpected response from DES, received status $status"))
+            }
+          }
+      }
+
     }
   }
 
