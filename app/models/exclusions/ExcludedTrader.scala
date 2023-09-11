f4cdcaaa046e4de8147578925cbfb25dcf3d2d58
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2023-09-11 13:31:01 +0100

    VEOSS-1713 Removed unnecessary function from ExcludedTrader

--- app/models/exclusions/ExcludedTrader.scala
+++ app/models/exclusions/ExcludedTrader.scala
@@ -24,17 +24,7 @@ case class ExcludedTrader(
                            vrn: Vrn,
                            exclusionReason: Int,
                            effectivePeriod: Period
-                         ){
-  val exclusionSource: String = derriveExclusionSource(exclusionReason)
-
-  private def derriveExclusionSource(code: Int) = {
-    code match {
-      case x if x == 2 || x == 4 => "HMRC"
-      case _ => "TRADER"
-    }
-  }
-}
-
+                         )
 object ExcludedTrader {
 
   implicit val format: OFormat[ExcludedTrader] = Json.format[ExcludedTrader]
