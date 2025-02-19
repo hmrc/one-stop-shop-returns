ee7807699c709ed90eb59356c5dcd0cf6cc64fc9
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-02-19 14:46:22 +0000

    VEOSS-2037 | Update dependencies as per Bobby warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,23 @@ import sbt.*
 
 object AppDependencies {
 
+  private val bootstrapVersion = "9.9.0"
+  private val hmrcMongoVersion = "2.5.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % "9.5.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc"             %% "domain-play-30"             % "10.0.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % "2.2.0"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "9.5.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % "2.2.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
     "org.scalatest"           %% "scalatest"                  % "3.2.19",
     "org.playframework"       %% "play-test"                  % PlayVersion.current,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1",
     "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
-    "org.scalatestplus"       %% "mockito-4-11"                % "3.2.18.0"
+    "org.scalatestplus"       %% "mockito-4-11"               % "3.2.18.0"
   ).map(_ % "test, it")
 }
